dd95fc97a3ec51dcd927ef29c578413077f0981e
rafidrahman1 <rafid0001@gmail.com>
2025-05-21 15:18:21 +0600

    feat: speed insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <SpeedInsights/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   );
