eefa84fbb2e39c47bb818cf718459b1ecb8676be
rafidrahman1 <rafid0001@gmail.com>
2025-05-26 15:49:27 +0600

    done some routing and stuff

---
+++ app/(projects)/agent/page.tsx
@@ -0,0 +1,12 @@
+
+
+const ProjectsLayout = () => {
+    return (
+        <div>
+
+            <button className="font-bold bg-amber-700">hello</button>
+
+        </div>
+    )
+}
+export default ProjectsLayout;
