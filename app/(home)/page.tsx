e4de4e0cf186cb3fa3bd96226f5a1b15bcfb586b
rafidrahman1 <rafid0001@gmail.com>
2025-06-03 16:46:00 +0600

    don no

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,11 @@
-import Hero from "../components/home/Hero";
+// import Hero from "../components/home/Hero";
 import Resume from "../components/home/Resume";
 import Projects from "../components/home/Projects";
 
 export default async function Home() {
   return (
   <div>
-    <Hero/>
+    {/*<Hero/>*/}
     <Resume/>
     <Projects/>
   </div>
